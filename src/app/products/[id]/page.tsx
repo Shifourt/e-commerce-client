0cc9811f98ada21d1f64839c41dec54827adf188
Shifat Hossain <shifatwhere@gmail.com>
2025-09-08 14:27:26 +0600

    test 2

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -21,7 +21,6 @@ const product: ProductType = {
 };
 
 export const generateMetadata = async ({
-  params,
 }: {
   params: { id: string };
 }) => {
@@ -29,21 +28,22 @@ export const generateMetadata = async ({
   // TEMPORARY
   return {
     title: product.name,
-    describe: product.description,
+    description: product.description,
   };
 };
 
-const ProductPage = async ({
-  params,
+const ProductPage = ({
   searchParams,
 }: {
   params: { id: string };
-  searchParams: { color?: string; size?: string };
+  searchParams?: { color?: string; size?: string };
 }) => {
-  const { size, color } = searchParams;
+  const size = searchParams?.size;
+  const color = searchParams?.color;
 
-  const selectedSize = size || (product.sizes[0] as string);
-  const selectedColor = color || (product.colors[0] as string);
+  const selectedSize = size || product.sizes[0];
+  const selectedColor = color || product.colors[0];
+
   return (
     <div className="flex flex-col gap-4 lg:flex-row md:gap-12 mt-12">
       {/* IMAGE */}
@@ -55,6 +55,7 @@ const ProductPage = async ({
           className="object-contain rounded-md"
         />
       </div>
+
       {/* DETAILS */}
       <div className="w-full lg:w-7/12 flex flex-col gap-4">
         <h1 className="text-2xl font-medium">{product.name}</h1>
@@ -65,35 +66,19 @@ const ProductPage = async ({
           selectedSize={selectedSize}
           selectedColor={selectedColor}
         />
+
         {/* CARD INFO */}
         <div className="flex items-center gap-2 mt-4">
-          <Image
-            src="/klarna.png"
-            alt="klarna"
-            width={50}
-            height={25}
-            className="rounded-md"
-          />
-          <Image
-            src="/cards.png"
-            alt="cards"
-            width={50}
-            height={25}
-            className="rounded-md"
-          />
-          <Image
-            src="/stripe.png"
-            alt="stripe"
-            width={50}
-            height={25}
-            className="rounded-md"
-          />
+          <Image src="/klarna.png" alt="klarna" width={50} height={25} className="rounded-md" />
+          <Image src="/cards.png" alt="cards" width={50} height={25} className="rounded-md" />
+          <Image src="/stripe.png" alt="stripe" width={50} height={25} className="rounded-md" />
         </div>
+
         <p className="text-gray-500 text-xs">
           By clicking Pay Now, you agree to our{" "}
           <span className="underline hover:text-black">Terms & Conditions</span>{" "}
-          and <span className="underline hover:text-black">Privacy Policy</span>
-          . You authorize us to charge your selected payment method for the
+          and <span className="underline hover:text-black">Privacy Policy</span>.
+          You authorize us to charge your selected payment method for the
           total amount shown. All sales are subject to our return and{" "}
           <span className="underline hover:text-black">Refund Policies</span>.
         </p>
